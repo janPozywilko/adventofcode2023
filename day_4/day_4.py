a8015e0e27a1c96435d621db68862158352a3ee6
janpozywilko <jasui99@gmail.com>
2023-12-04 22:43:33 +0100

    One star with frozen feets and beer in hand

--- day_4/day_4.py
+++ day_4/day_4.py
@@ -1,7 +1,33 @@
 import typer
 
+def loadData():
+    with open("data.txt", "r") as f:
+        d = f.read().splitlines()
+    return d
+
+def countPoints(card):
+    winning_numbers, numbers = card.split(":")[1].split("|")
+    # Count how many numbers are in the winning numbers
+    winning_numbers = winning_numbers.split()
+    numbers = numbers.split()
+    # count how many items from numbers are in winning_numbers
+    result = 0
+    for n in numbers:
+        if n in winning_numbers:
+            result += 1
+    if result != 0:
+        return 2**(result-1)
+    return 0
+
+
+
+
 def main():
-    pass
+    data = loadData()
+    result = 0
+    for card in data:
+        result += countPoints(card=card)
+    print(result)
 
 
 if __name__ == "__main__":
