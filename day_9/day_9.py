a84b9b33a6929e7a584bd6ad7a7ebc4016b5ac5e
janpozywilko <jasui99@gmail.com>
2023-12-11 19:42:56 +0100

    Day 9 seemed to easy

--- day_9/day_9.py
+++ day_9/day_9.py
@@ -12,14 +12,25 @@ def predict(numbers):
         return numbers[-1] + diffs[0]
     return numbers[-1] + predict(diffs)
     
-
+def predictBack(numbers):
+    diffs = [numbers[i+1] - numbers[i] for i in range(len(numbers)-1)]
+    if len(set(diffs)) == 1:
+        return numbers[0] - diffs[0]
+    return numbers[0] - predictBack(diffs)
+    
 
 def main():
     data = loadData("data.txt")
     res = 0
+    res2 = 0
+    # for row in data:
+    #     res += predict([int(x) for x in row.split()])
+    
+    # print(f'Part 1:{res}')
+
     for row in data:
-        res += predict([int(x) for x in row.split()])
-    print(res)
+        res2 += predictBack([int(x) for x in row.split()])
+    print(res2)
 
 if __name__ == "__main__":
     typer.run(main)
